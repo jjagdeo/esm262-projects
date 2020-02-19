5be6f9e6ceffe2a1da635891aeb7efed01f4eb8b
Jessica Jagdeo <jessica32703@gmail.com>
2020-02-19 10:31:18 -0800

    Working on Darcy's Law function and Assignment 1 during class

--- working/darcy_law.R
+++ working/darcy_law.R
@@ -1,4 +1,11 @@
 darcy_law = function(hydraulic_conductivity, cross_section_area, hydraulic_head) {
+  
+  if (cross_section_area < 0) return("Cross section area cannot be negative")
+  
+  if (hydraulic_head > 1000000) return("Reconsider hydraulic head value")
+  
   result = -hydraulic_conductivity*cross_section_area*hydraulic_head
+  
   return(result)
+  
 }
